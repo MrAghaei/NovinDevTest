fb610831d995f4c983426ddb4095a6e0786814ee
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-28 12:44:49 +0330

    [#6-ADD] detail page

--- src/pages/user-list/UserListPage.tsx
+++ src/pages/user-list/UserListPage.tsx
@@ -8,7 +8,7 @@ import { DEFAULT_PAGE_SIZE } from "@/configs/pageable.config.ts";
 
 function UserListPage() {
   //region hooks
-  const { fetchUsersData, usersData } = useUser();
+  const { fetchUsersData, usersData, isAllUsersFetchLoading } = useUser();
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -34,6 +34,9 @@ function UserListPage() {
   }
   //endregion
 
+  if (isAllUsersFetchLoading) {
+    return <div className={"flex justify-center"}>Loading...</div>;
+  }
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-8">
